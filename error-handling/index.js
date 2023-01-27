72f2325718ba205c41bdb916c2ec146f29400e35
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-01-27 08:36:28 +0000

    add new err message

--- error-handling/index.js
+++ error-handling/index.js
@@ -9,6 +9,11 @@ module.exports = (app) => {
     // always logs the error
     console.error("ERROR", req.method, req.path, err);
 
+    //invalid token
+    if (err.name === "UnauthorizedError") {
+      res.status(401).json({ message: "invalid token..." });
+    }
+
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
       res.status(500).json({
